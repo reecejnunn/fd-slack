68fb845e40d78a40502879476ebfa531c23ec339
Lucy <lucy@lucymhdavies.com>
2017-04-03 17:01:27 +0100

    s/log.debug/logger.debug/

--- cmd/delay.rb
+++ cmd/delay.rb
@@ -1,12 +1,12 @@
 
 def delay
-	log.debug "Delayed message test"
+	logger.debug "Delayed message test"
 
 	task = Thread.new {
-		log.debug "sleeping for 2 secs"
+		logger.debug "sleeping for 2 secs"
 		sleep(2)
 		post_data = slack_secret_message( "Second message" )
-		log.debug "Posting to #{params['response_url']}, #{post_data}"
+		logger.debug "Posting to #{params['response_url']}, #{post_data}"
 		RestClient.post(params['response_url'], post_data )
 	}
 

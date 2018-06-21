4610cf03d7db90148399ed537169c4726fa70052
Oliver Leaver-Smith <oliver.leaver-smith@skybettingandgaming.com>
2018-06-21 10:31:08 +0100

    change commands

--- app.rb
+++ app.rb
@@ -1,20 +1,3 @@
-# TODO: Refactor the balls out of this
-# My current thinking is:
-# - Have a directory, e.g. 'commands', in which all command code is stored
-# -- e.g. coffee_roulette
-# - Have a hash for which commands are available
-# -- Interact with this through a Class, e.g.
-# -- LAAS.Commands.register()
-# -- This should define:
-# --- command name
-# --- help text
-# --- output method (stdout/stderr = default (in_channel, secret), or by specifying pair)
-# -- Maybe each command should be it's own class
-
-
-# TODO: Avatar (square)
-# i.e. just pick it from gravatar
-
 before do
 	# Based on http://stackoverflow.com/a/26675418
 	unless ( request.secure? || Sinatra::Application.environment != :production )
@@ -30,8 +13,6 @@ get '/slack-slash' do
 	"Yo. You probably meant to POST to this URL, right?"
 end
 
-# TODO: Verify Slack auth token
-# Wrapper for the main /laas slash command
 post '/slack-slash' do
 	logger.debug(__method__){ "New request: #{params.inspect}" }
 
@@ -40,56 +21,26 @@ post '/slack-slash' do
 	end
 
 	begin
-		# TODO: pick these from a hash
 		case params['text'].split.first
 		when "", nil
-			what_is_laas
+			what_is_fd
 		when "help"
 			slack_secret_message help
-		when "iou"
-			slack_secret_message iou
-		when "quote"
-			quote
-		when "getRandomNumber"
-			slack_message xkcd221
-		when "teaflick"
-			slack_message teaflick
-		when "save"
-			save_message
-		when "replay"
-			replay_message
-		when "lunch_roulette"
-			slack_message lunch_roulette
-		when "coffee", "coffee_roulette"
-			coffee_roulette
-		when "noodle", "noodle_roulette"
-			slack_message noodle_roulette
-		when "standup"
-			standup
-		when "monkey", "monkeys"
-			monkey
-		when "summon"
-			slack_message summon
-		when "delay"
-			delay
-		when "smeg"
-			red_dwarf_quote
-		when "big_text"
-			big_text
-		when "clear"
-			clear
-		when "admin", "sudo"
-			admin
-		when "donut"
-			donut
-		when "say_raw"
-			say_raw
+		when "start"
+			task = Thread.new {
+				post_data = slack_message( "Firedrill *start*" )
+				RestClient.post(params['response_url'], post_data )
+			}
+		when "stop"
+			task = Thread.new {
+				post_data = slack_message( "Firedrill *stop*" )
+				RestClient.post(params['response_url'], post_data )
+			}
 		when "say"
-			say_message
-		when "isay"
-			me_say_message
-		when "version"
-			version
+			task = Thread.new {
+				post_data = slack_message( "*#{params['text'].split[1].upcase}* #{params['text'].split[2..-1].join(' ')}" )
+				RestClient.post(params['response_url'], post_data )
+			}
 		else
 			slack_secret_message "I don't know what to do with: #{params['text'].split.first}"
 		end
--- cmd/delay.rb
+++ cmd/delay.rb
@@ -1,14 +1,10 @@
 
 def delay
-	log.debug "Delayed message test"
 
 	task = Thread.new {
-		log.debug "sleeping for 2 secs"
 		sleep(2)
-		post_data = slack_secret_message( "Second message" )
-		log.debug "Posting to #{params['response_url']}, #{post_data}"
+		post_data = slack_message( "Second message" )
 		RestClient.post(params['response_url'], post_data )
 	}
 
-	slack_secret_message "First message"
 end
